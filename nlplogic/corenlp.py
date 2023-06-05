8b819e494c72d8605a146f9ba069d108ea510b53
CinnamonrollSaigo <121988836+CinnamonrollSaigo@users.noreply.github.com>
2023-06-05 14:57:14 +0000

    some add

---
+++ nlplogic/corenlp.py
@@ -0,0 +1,26 @@
+from textblob import TextBlob
+import wikipedia
+
+def search_wikipedia(name):
+    """search"""
+    print(f"searching for name:{name}")
+    return wikipedia.search(name)
+
+
+def summary_wikepedia(name):
+    """summary"""
+    print(f"summary for name:{name}")
+    return wikipedia.summary(name)
+
+
+def get_text_blob(text):
+    """get text blob"""
+    blob = TextBlob(text)
+    return blob
+
+def get_phrases(name):
+    """wikipedia"""
+    text = summary_wikepedia(name)
+    blob = get_text_blob(text)
+    phrases = blob.noun_phrases
+    return phrases
